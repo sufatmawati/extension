e9d604775ab05953973252ba4aa26ea570790184
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-02-12 15:34:30 +0000

    chore: use tokens instead of consts to be more reliable

--- theme/tokens.ts
+++ theme/tokens.ts
@@ -4,4 +4,16 @@ import { defineTokens } from '@pandacss/dev';
 // ts-unused-exports:disable-next-line
 export const tokens = defineTokens({
   ...leatherTokens,
+
+  sizes: {
+    ...leatherTokens.sizes,
+    // 4370 TODO - update in monorepo and deprecate - centeredPageFullWidth
+    pageWidth: { value: '500px' },
+    twoColumnPageWidth: { value: '500px' },
+    fullPageMaxWidth: { value: '882px' },
+    dialogHeight: { value: '600px' },
+    headerHeight: { value: '80px' },
+    headerHeightSm: { value: '68px' },
+    footerHeight: { value: '95px' },
+  },
 });
