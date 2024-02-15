Eshwari007/Test-extension

chore: replace drawer with dialog, add containers and global header footers, refactor onboarding, ref #4371

tests/page-object-models/send.page.ts
@@ -85,7 +85,7 @@ export class SendPage {
   }
 
   async goBack() {
-    await this.page.getByTestId(SharedComponentsSelectors.ModalHeaderBackBtn).click();
+    await this.page.getByTestId(SharedComponentsSelectors.PageHeaderBackBtn).click();
   }
 
   async goBackSelectStx() {

sufatmawati/extension

tests/page-object-models/send.page.ts
@@ -85,7 +85,7 @@ export class SendPage {
   }
 
   async goBack() {
-    await this.page.getByTestId(SharedComponentsSelectors.ModalHeaderBackBtn).click();
+    await this.page.getByTestId(SharedComponentsSelectors.DialogHeaderBackBtn).click();
   }
 
   async goBackSelectStx() {