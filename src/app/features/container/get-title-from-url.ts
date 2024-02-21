fbc712f88ee111840be446933d8935743e753121
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-02-21 15:24:50 +0000

    chore: clean code

--- src/app/features/container/get-title-from-url.ts
+++ src/app/features/container/get-title-from-url.ts
@@ -1,14 +1,5 @@
 import { RouteUrls } from '@shared/route-urls';
 
-// getTitleFromUrl is not ideal but a stepping stone solution for now to deprecate useRouteHeader
-// TODO
-// - test this work with `/` or nested routes or if we need to use match() to be safe
-// - investigate having title as a route param
-
-//  FIXME - these titles look a bit off, not sure if we should remove them and add them into the `Page`
-// its tricky with the current design as sometimes titles in Page inside layout and sometimes in header. Keeping them to header for now
-
-// TODO 4370 task #3 sort out consistent headers. For now keeping in main header not page
 export function getTitleFromUrl(pathname: RouteUrls) {
   if (pathname.match(RouteUrls.SendCryptoAsset)) return 'Send';
 
@@ -21,7 +12,6 @@ export function getTitleFromUrl(pathname: RouteUrls) {
       return 'Locked Bitcoin';
     case RouteUrls.SendBrc20ChooseFee:
       return 'Choose fee';
-    // TODO rename these routes so they are consistent - SomethingReview
     case RouteUrls.SendBrc20Confirmation:
     case RouteUrls.SwapReview:
     case RouteUrls.SendBrc20Confirmation:
@@ -29,7 +19,6 @@ export function getTitleFromUrl(pathname: RouteUrls) {
       return 'Review';
     case RouteUrls.Swap:
       return 'Swap';
-    // TODO check these summarys as I think the UI is different now and title should be inside card
     case RouteUrls.SentStxTxSummary:
     case RouteUrls.SentBtcTxSummary:
       return 'Sent';
