9a11eeba5ca88bf085ed8a45dc70a9280a278a65
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-02-23 10:06:21 +0000

    chore: move Toast up hierarchy so it appears above radix dialog and other content

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from 'react';
+import { Toaster } from 'react-hot-toast';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -32,6 +33,12 @@ export function App() {
             </Suspense>
           </QueryClientProvider>
         </ThemeSwitcherProvider>
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            style: { fontSize: '14px' },
+          }}
+        />
       </PersistGate>
     </ReduxProvider>
   );
