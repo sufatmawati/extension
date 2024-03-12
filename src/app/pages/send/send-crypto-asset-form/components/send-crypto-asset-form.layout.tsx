f38ab360c57050b7bdc55882d07101bb5bbc66cf
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-03-12 09:26:05 +0000

    chore: make height of send asset more dynamic

--- src/app/pages/send/send-crypto-asset-form/components/send-crypto-asset-form.layout.tsx
+++ src/app/pages/send/send-crypto-asset-form/components/send-crypto-asset-form.layout.tsx
@@ -1,23 +1,23 @@
 import { SendCryptoAssetSelectors } from '@tests/selectors/send.selectors';
 import { Flex } from 'leather-styles/jsx';
+import { token } from 'leather-styles/tokens';
 
-import { CardContent } from '@app/ui/layout/card/card-content';
+import { HasChildren } from '@app/common/has-children';
 
-interface SendCryptoAssetFormLayoutProps {
-  children: React.ReactNode;
-}
-export function SendCryptoAssetFormLayout({ children }: SendCryptoAssetFormLayoutProps) {
+export function SendCryptoAssetFormLayout({ children }: HasChildren) {
   return (
-    <CardContent>
-      <Flex
-        alignItems="center"
-        data-testid={SendCryptoAssetSelectors.SendForm}
-        flexDirection="column"
-        p="space.05"
-        pt="space.06"
-      >
-        {children}
-      </Flex>
-    </CardContent>
+    <Flex
+      alignItems="center"
+      data-testid={SendCryptoAssetSelectors.SendForm}
+      flexDirection="column"
+      p="space.05"
+      pt="space.06"
+      width="100%"
+      overflowY="auto"
+      style={{ marginBottom: token('sizes.footerHeight') }}
+      maxHeight={{ base: '70vh', md: '80vh' }}
+    >
+      {children}
+    </Flex>
   );
 }
