36f48be6fe2eb02852e8b159fbb01d9f181541b9
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-03-13 15:35:17 +0000

    chore: don't allow users to go back from summary pages

--- src/app/features/container/utils/route-helpers.ts
+++ src/app/features/container/utils/route-helpers.ts
@@ -34,8 +34,20 @@ export function getIsSessionLocked(pathname: RouteUrls) {
   return pathname === RouteUrls.Unlock;
 }
 
+function isSummaryPage(pathname: RouteUrls) {
+  /* TODO refactor the summary routes to make this cleaner
+  we need to block going back from summary pages catching the dynamic routes:
+  SentBtcTxSummary = '/sent/btc/:txId',
+  SentStxTxSummary = '/sent/stx/:txId',
+  SentBrc20Summary = '/send/brc20/:ticker/summary',
+  RpcSignPsbtSummary = '/sign-psbt/summary',
+  RpcSendTransferSummary = '/send-transfer/summary',
+  */
+  return pathname.match('/sent/stx/') || pathname.match('/sent/btc/' || pathname.match('summary'));
+}
+
 export function canGoBack(pathname: RouteUrls) {
-  if (getIsSessionLocked(pathname) || isKnownPopupRoute(pathname)) {
+  if (getIsSessionLocked(pathname) || isKnownPopupRoute(pathname) || isSummaryPage(pathname)) {
     return false;
   }
   return true;
